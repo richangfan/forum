0f5b4c9a598c393c86b68cc35e38f8e7372f430e
richangfan <richangfan@outlook.com>
2021-04-10 21:21:08 +0800

    fix

--- routes/post.go
+++ routes/post.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"encoding/json"
 	"richangfan/forum/tool"
+	"unicode/utf8"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,6 +26,12 @@ func AddPost(rg *gin.RouterGroup) {
 		err = json.Unmarshal(raw, &p)
 		if err != nil {
 			tool.SendErrorJson(c, err.Error())
+			return
+		}
+		tlen := utf8.RuneCountInString(p.Title)
+		clen := utf8.RuneCountInString(p.Content)
+		if tlen == 0 || tlen > 50 || clen == 0 || clen > 10000 {
+			tool.SendErrorJson(c, "输入出错")
 			return
 		}
 		tool.SendSuccessJson(c, nil)
